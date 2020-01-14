ca0559534b2daebc65718f0d37bd0cb89215735c
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-01-14 11:02:12 +0700

    complete submission 1

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -12,7 +12,8 @@ namespace BooksCatalogue.Controllers
     {
         private string apiEndpoint = "https://bookscatalogueapi-dicoding.azurewebsites.net/api/";
 
-        public ReviewController() {
+        public ReviewController()
+        {
         }
 
         // GET: Review/AddReview/2
@@ -28,7 +29,7 @@ namespace BooksCatalogue.Controllers
 
             HttpResponseMessage response = await client.SendAsync(request);
 
-            switch(response.StatusCode)
+            switch (response.StatusCode)
             {
                 case HttpStatusCode.OK:
                     string responseString = await response.Content.ReadAsStringAsync();
@@ -43,12 +44,38 @@ namespace BooksCatalogue.Controllers
             }
         }
 
-        // TODO: Tambahkan fungsi ini untuk mengirimkan atau POST data review menuju API
         // POST: Review/AddReview
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddReview([Bind("Id,BookId,ReviewerName,Rating,Comment")] Review review)
         {
+
+            if (ModelState.IsValid)
+            {
+                var httpContent = new[] {
+                    new KeyValuePair<string, string>("bookId", review.BookId.ToString()),
+                    new KeyValuePair<string, string>("reviewerName", review.ReviewerName),
+                    new KeyValuePair<string, string>("rating", review.Rating.ToString()),
+                    new KeyValuePair<string, string>("comment", review.Comment)
+                };
+
+                HttpClient client = new HttpClient();
+                HttpContent content = new FormUrlEncodedContent(httpContent);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, apiEndpoint + "review/");
+                request.Content = content;
+
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                    case HttpStatusCode.NoContent:
+                    case HttpStatusCode.Created:
+                        return Redirect("/Books/Details/" + review.BookId);
+                    default:
+                        return ErrorAction("Error. Status code = " + response.StatusCode);
+                }
+            }
 
             return View(review);
         }
